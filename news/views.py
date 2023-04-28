5740f8f7617565b679406ac5a78a368af1280f65
brocode <kelimbth@gmail.com>
2023-04-28 14:10:49 +0300

    created homepage view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+import requests
+import environ
 
-# Create your views here.
+env = environ.Env()
+environ.Env.read_env()
+
+
+class HomepageView(View):
+    def get(self, request):
+        API_KEY = env('API_KEY')
+        BASE_URL = f'https://newsapi.org/v2/everything?q=country&apiKey={API_KEY}'
+        response = requests.get(BASE_URL)
+        data = response.json()
+
+        news_articles = data['articles'][:2]
+
+        context = {'news_articles': news_articles}
+        return render(request, 'news/index.html', context)
