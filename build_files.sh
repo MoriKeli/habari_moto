05d4fc6c9b8e1aff81f9c2950113b7969a9aba74
brocode <kelimbth@gmail.com>
2023-04-29 02:15:07 +0300

    shell script

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+# build_files.sh
+echo "BUILD START"
+
+echo "INSTALLING PACKAGES ..."
+python3.9 -m pip install -r requirements.txt
+
+echo "COLLECTING STATIC FILES"
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
