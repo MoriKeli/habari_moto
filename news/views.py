a127dc6281a49e5e70f9964eb3c23b6cbc4b5f2c
brocode <kelimbth@gmail.com>
2023-04-28 15:12:49 +0300

    added pagination

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
@@ -15,7 +16,23 @@ class HomepageView(View):
         response = requests.get(BASE_URL)
         data = response.json()
 
-        news_articles = data['articles'][:2]
+        news_articles = data['articles']
 
-        context = {'news_articles': news_articles}
+        # pagination
+        p = Paginator(list(news_articles), 10) 
+        page_number = request.GET.get('page')
+
+        try:
+            page_obj = p.get_page(page_number)
+
+        except PageNotAnInteger:
+            # if page_number is not an integer then assign the first page
+            page_obj = p.page(1)
+        except EmptyPage:
+            # if page is empty then return last page
+            page_obj = p.page(p.num_pages)
+
+        context = {
+            'news_articles': page_obj, 'page': page_obj,
+            }
         return render(request, 'news/index.html', context)
