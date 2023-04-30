43eeef47f1aaecd52ae028944b31d5b9b89f5b02
brocode <kelimbth@gmail.com>
2023-04-30 10:38:33 +0300

    reverting to initial shell commands

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,7 @@ echo "BUILD START"
 echo "Installing modules ..."
 python3.9 -m pip install -r requirements.txt
 
+echo "collecting static files ..."
+python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
