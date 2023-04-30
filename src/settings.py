2c3d3b99e2b74f7f46e2254baaff9ebe1a2d2bdb
brocode <kelimbth@gmail.com>
2023-04-30 11:32:08 +0300

    updated config.

--- src/settings.py
+++ src/settings.py
@@ -15,9 +15,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['globalnews.onrender.com']
 
 
 # Application definition
