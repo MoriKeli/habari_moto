197b54e0226c45dbbe04864dbc332cf7a69325d5
brocode <kelimbth@gmail.com>
2023-04-30 09:53:50 +0300

    minor change

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 # build_files.sh
 echo "BUILD START"
 
-echo "INSTALLING PACKAGES ..."
+echo "Installing modules ..."
 python3.9 -m pip install -r requirements.txt
+
+echo "collecting static files ..."
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
