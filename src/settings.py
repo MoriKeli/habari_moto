6561b5946897d1290c2887925ec6f522b9ae31b5
brocode <kelimbth@gmail.com>
2023-04-30 11:40:03 +0300

    updated ALLOWED HOSTS & DEBUG

--- src/settings.py
+++ src/settings.py
@@ -15,9 +15,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['globalnews.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
